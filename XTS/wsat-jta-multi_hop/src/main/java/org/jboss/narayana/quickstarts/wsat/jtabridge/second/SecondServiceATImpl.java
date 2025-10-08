cc8020274fa52227664b2a0f6bcef124332da167
Joerg Baesner <jbaesner@redhat.com>
2025-10-08 13:52:10 +0200

    removed custom HandlerChain

--- XTS/wsat-jta-multi_hop/src/main/java/org/jboss/narayana/quickstarts/wsat/jtabridge/second/SecondServiceATImpl.java
+++ XTS/wsat-jta-multi_hop/src/main/java/org/jboss/narayana/quickstarts/wsat/jtabridge/second/SecondServiceATImpl.java
@@ -1,6 +1,5 @@
 package org.jboss.narayana.quickstarts.wsat.jtabridge.second;
 
-import jakarta.jws.HandlerChain;
 import org.jboss.narayana.quickstarts.wsat.jtabridge.second.jaxws.SecondServiceAT;
 
 import jakarta.ejb.Remote;
@@ -18,7 +17,6 @@ import jakarta.persistence.PersistenceContext;
  */
 @Stateless
 @Remote(SecondServiceAT.class)
-@HandlerChain(file = "/context-handlers.xml")
 @WebService(serviceName = "SecondServiceATService", portName = "SecondServiceAT", name = "SecondServiceAT", targetNamespace = "http://www.jboss.org/narayana/quickstarts/wsat/simple/second")
 @SOAPBinding(style = SOAPBinding.Style.RPC)
 @TransactionAttribute(TransactionAttributeType.MANDATORY) // default is REQUIRED
--- XTS/wsat-jta-multi_hop/src/test/java/org/jboss/narayana/quickstarts/wsat/jtabridge/fromjta/BridgeFromJTATest.java
+++ XTS/wsat-jta-multi_hop/src/test/java/org/jboss/narayana/quickstarts/wsat/jtabridge/fromjta/BridgeFromJTATest.java
@@ -47,7 +47,6 @@ public class BridgeFromJTATest {
                 .addPackages(true, FirstServiceATImpl.class.getPackage())
                 .addPackages(true, SecondServiceATImpl.class.getPackage())
                 .addPackages(true, FirstClient.class.getPackage())
-                .addAsResource(new File("src/main/resources/context-handlers.xml"))
                 .addAsWebInfResource(new File("src/main/resources/META-INF/persistence.xml"), "classes/META-INF/persistence.xml");
 
         archive.setManifest(new StringAsset(ManifestMF));
